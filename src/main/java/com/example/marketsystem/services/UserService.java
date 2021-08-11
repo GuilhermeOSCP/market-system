01f4a905bb2be0ba5f2e700a29925bf04f1dde69
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-08-10 23:18:48 -0300

    Implementacao Exception handling - delete

--- src/main/java/com/example/marketsystem/services/UserService.java
+++ src/main/java/com/example/marketsystem/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.marketsystem.entities.User;
 import com.example.marketsystem.repositories.UserRepository;
+import com.example.marketsystem.services.exceptions.DatabaseException;
 import com.example.marketsystem.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
